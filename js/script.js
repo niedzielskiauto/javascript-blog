0ab5348fdb112ea45d496d03949eb8b84c9b196f
Kamil Niedzielski <kamil.niedzielski@icloud.com>
2021-12-09 12:02:19 +0100

    Done find the correct article using the selector

--- js/script.js
+++ js/script.js
@@ -34,8 +34,12 @@
 
     console.log(articleSelector);
 
-    /* find the correct article using the selector (value of 'href' attribute) */
-  
+    /* [Done] find the correct article using the selector (value of 'href' attribute) */
+    const targetArticle = document.querySelector(articleSelector);
+
+    console.log(targetArticle);
+
+
     /* add class 'active' to the correct article */
   }
   
