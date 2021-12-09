e4a52b57a8aee164d10d9608e05119f9df75f0d5
Kamil Niedzielski <kamil.niedzielski@icloud.com>
2021-12-09 17:37:10 +0100

    Problem with clicking on link

--- js/script.js
+++ js/script.js
@@ -43,11 +43,9 @@ const titleClickHandler = function (event) {
   targetArticle.classList.add("active");
 }
 
-const links = document.querySelectorAll(".titles a");
 
-for (let link of links) {
-  link.addEventListener("click", titleClickHandler);
-}
+
+  
 
 const optArticleSelector = ".post",
   optTitleSelector = ".post-title",
@@ -62,22 +60,31 @@ function generateTitleLinks(){
 
   /* [Done[ for each article */
   const articles = document.querySelectorAll(optArticleSelector);
+  
+  let html = '';
 
   for(let article of articles) {
-    article.querySelector(optArticleSelector);
-  }
-  console.log(articles);
-
-
-    /* get the article id */
+    /* [In progress] get the article id */
+    const articleId = article.getAttribute('id');
 
     /* find the title element */
-
     /* get the title from the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
 
     /* create HTML of the link */
+    const linkHTML = `<li><a href="#"${articleId}><span>${articleTitle}</span></a></li>`;
 
     /* insert link into titleList */
+    html = html + linkHTML;
+  }
+  
+
+  titleList.innerHTML = html;
+
+  const links = document.querySelectorAll(".titles a");
+  for (let link of links) {
+    link.addEventListener('click', titleClickHandler);
+  }
 
 }
 
