5f59cedda5b4f4c08bb1af954d1a0745aae4d794
Kamil Niedzielski <kamil.niedzielski@icloud.com>
2021-12-09 11:23:56 +0100

    Add class .active to the clicked link

--- js/script.js
+++ js/script.js
@@ -8,24 +8,25 @@
     console.log('Link was clicked!');
     console.log(event);
   
-    /* remove class 'active' from all article links  */
+    /* [Done] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
       activeLink.classList.remove('active');
     }
 
-    /* add class 'active' to the clicked link */
-  
-    /* remove class 'active' from all articles */
-  
+    /* [In progress] add class 'active' to the clicked link */
+    const clickedElement = this;
+    clickedElement.classList.add('active');
+    console.log('clickedElement (with plus): ',clickedElement);
+    
+
+    /* [Done] remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts .active');
 
     for(let activeArticle of activeArticles){
       activeArticle.classList.remove('active');
     }
-
-
 
     /* get 'href' attribute from the clicked link */
   
@@ -36,6 +37,6 @@
   
   const links = document.querySelectorAll('.titles a');
   
-  for(let link of links){
-    link.addEventListener('click', titleClickHandler);
-  }
+    for(let link of links){
+      link.addEventListener('click', titleClickHandler);
+    }
